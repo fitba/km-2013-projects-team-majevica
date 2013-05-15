0d90ae19148531d6b3241b26bbcafa9c36b07334
dzenan kukrica <dzenan.kukrica@edu.fit.ba>
2013-05-15 13:36:17 +0200

    comments and css

--- src/FITkms/Controllers/UserController.cs
+++ src/FITkms/Controllers/UserController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FITkms.Models;
 
 namespace FITkms.Controllers
 {
     public class UserController : Controller
     {
+
         //
         // GET: /User/
 
@@ -15,8 +17,28 @@ namespace FITkms.Controllers
         {
             return View();
         }
+
+        [HttpGet]
         public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(int broj = 5)
+        {
+            var model = new UserModel();
+            var username = Request["txtusername"];
+            var password = Request["txtpassword"];
+            if (model.GetLogin(username, password) != null)
+            {
+                //dodaj u sesiju
+                Response.Redirect("/");
+            }
+            else
+            {
+                Response.Redirect("/user/login");
+            }
             return View();
         }
 
---
+++ src/FITkms/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using FITkms.Models;
+
+namespace FITkms.Models
+{
+    public class UserModel
+    {
+
+        public Users GetLogin(string username, string password)
+        {
+            kmsEntities db = new kmsEntities();
+            return db.Users.Where(x => x.Username == username && x.Password == password).SingleOrDefault();
+        }
+    }
+}
